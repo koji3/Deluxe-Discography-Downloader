8c13e0f35010411352609e024a03070b0401e0c8
koji3 <jose.linares.1993@gmail.com>
2016-01-31 14:47:42 +0100

    Fixed little bug

--- ddd.sh
+++ ddd.sh
@@ -3,6 +3,21 @@
 # $1: artista a buscar
 # necesario youtube-dl jshon
 
+rojo="\033[0;31m"
+verde="\033[0;32m"
+azul="\033[0;34m"
+magenta="\033[0;35m"
+cyan="\033[01;36m"
+grisC="\033[0;37m"
+gris="\033[1;30m"
+rojoC="\033[1;31m"
+verdeC="\033[1;32m"
+amarillo="\033[1;33m"
+azulC="\033[1;34m"
+magentaC="\033[1;35m"
+cyanC="\033[1;36m"
+blanco="\033[1;37m"
+
 # Busca artistas y los muestra ordenados para dejar que el usuario seleccione el correcto. 
 # $1= cadena a buscar
 function buscar_artista()
@@ -20,7 +35,7 @@ function buscar_artista()
 	else
 		read -p "Introduce el id del artista deseado ( 0 - $(($num_resultados-1)) ) " id_artista_seleccionado
 	fi
-	buscar_discografia $(cat json_temp_artistas | jshon -e artists -e $id_artista_seleccionado -e name -u)
+	buscar_discografia "$(cat json_temp_artistas | jshon -e artists -e $id_artista_seleccionado -e name -u)"
 }
 
 # Busca todos los albumes del artista pasado por parametro
@@ -28,7 +43,8 @@ function buscar_artista()
 function buscar_discografia()
 {
 	json_temp_albums=$(mktemp)
-	curl -s 'https://www.syotify.com/get-artist?top-tracks=true' -H 'origin: https://www.syotify.com' -H 'accept-encoding: gzip, deflate' -H 'accept-language: es-ES,es;q=0.8' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36' -H 'content-type: application/json;charset=UTF-8' -H 'accept: application/json, text/plain, */*' -H 'cache-control: no-cache' -H 'authority: www.syotify.com' -H 'referer: https://www.syotify.com/search/extremoduro' --data-binary '{"name":"'$1'"}' --compressed > json_temp_albums
+
+	curl -s 'https://www.syotify.com/get-artist?top-tracks=true' -H 'origin: https://www.syotify.com' -H 'accept-encoding: gzip, deflate' -H 'accept-language: es-ES,es;q=0.8' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36' -H 'content-type: application/json;charset=UTF-8' -H 'accept: application/json, text/plain, */*' -H 'cache-control: no-cache' -H 'authority: www.syotify.com' -H 'referer: https://www.syotify.com/search/extremoduro' --data-binary "{\"name\":\"$1\"}" --compressed > json_temp_albums
 	num_albums=$(cat json_temp_albums | jshon -e albums -l )
 	for x in $(seq 0 $(($num_albums-1)) ) ; do
 		echo "$x) " $(cat json_temp_albums | jshon -e albums -e $x -e name -u)
@@ -49,6 +65,7 @@ function descargar_album()
 	if [[ $1 -lt $num_albums ]] ; then
 		num_canciones=$(cat json_temp_albums | jshon -e albums -e $1 -e tracks -l)
 		nombre_album=$(cat json_temp_albums | jshon -e albums -e $1 -e name -u)
+		echo -e "$verde Descargando $azul $nombre_album"
 		mkdir -p "$nombre_artista/$nombre_album"
 		for x in $( seq 0 $(($num_canciones-1)) ) ; do
 			nombre_cancion=$(cat json_temp_albums | jshon -e albums -e $1 -e tracks -e $x -e name -u)
@@ -73,8 +90,8 @@ function descargar_album()
 # 
 function descargar_cancion()
 {
-	echo "[$1] Descargando $2"
-	youtube-dl -q --audio-format "mp3" --id -x $1
+	echo -e "$verde [ $rojo $1 $verde ] Descargando canción $amarillo $2"
+	youtube-dl -q --audio-format "mp3" --id -x "$1"
 	# Editar etiquetas id3
 	mv $1.mp3 "$3/$2.mp3"
 }
